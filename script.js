7225d7f5c0aea2f072e168622ab81850be90bbd8
Vishal Kumar <98923664+ritsvishalcs@users.noreply.github.com>
2025-05-25 16:16:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
-document.addEventListner("DomContentLoaded",function(){
+document.addEventListener("DomContentLoaded",function(){
 	document.body.innerText = "DOM load success"
 })
